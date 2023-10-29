354231e997d847876c05bcb6dd97a629bdc52e8a
muhammadkhanusmanov <uqaxromon@gmail.com>
2023-10-29 21:29:05 +0500

    added url

--- core/urls.py
+++ core/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from api.views import UserView, RegisterView
+from api.views import UserView, RegisterView,LogView,Logout
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/',UserView.as_view()),
     path('signup/', RegisterView.as_view()),
+    path('login/', LogView.as_view()),
+    path('logout/', Logout.as_view()),
 ]
