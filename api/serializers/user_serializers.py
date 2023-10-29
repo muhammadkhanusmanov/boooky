87d1456d5e1284b737a7c2c9875f585db67f7302
muhammadkhanusmanov <uqaxromon@gmail.com>
2023-10-29 13:55:51 +0500

    serializers

--- api/serializers/user_serializers.py
+++ api/serializers/user_serializers.py
@@ -1,1 +1,16 @@
 from rest_framework.serializers import ModelSerializer
+from django.contrib.auth.models import User
+from ..models import (
+    Staff
+)
+
+class StaffSerializer(ModelSerializer):
+    class Meta:
+        model = Staff
+        fields = '__all__'
+
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'first_name', 'last_name', 'email')
+
