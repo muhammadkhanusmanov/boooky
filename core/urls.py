53cea4f56df9abe9f68eea60576ddf9eac449ef9
muhammadkhanusmanov <uqaxromon@gmail.com>
2023-10-31 00:02:35 +0500

    enpoint for avatar view

--- core/urls.py
+++ core/urls.py
@@ -1,22 +1,6 @@
-"""
-URL configuration for core project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
-from api.views import UserView, RegisterView,LogView,Logout
+from api.views import UserView, RegisterView,LogView,Logout, AddImage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +8,5 @@ urlpatterns = [
     path('signup/', RegisterView.as_view()),
     path('login/', LogView.as_view()),
     path('logout/', Logout.as_view()),
+    path('addavatar/',AddImage.as_view())
 ]
