efddd5148454ee669051f5df6f2a8441eb277466
muhammadkhanusmanov <uqaxromon@gmail.com>
2023-10-31 20:17:35 +0500

    i

--- core/settings.py
+++ core/settings.py
@@ -74,11 +74,9 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     ],
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-    ]
 }
 
 # Database
