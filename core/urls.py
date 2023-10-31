db8cdefbf1c43bc8c1aa17350dffcd388790f597
muhammadkhanusmanov <uqaxromon@gmail.com>
2023-10-31 19:38:57 +0500

    enpoint

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from api.views import UserView, RegisterView,LogView,Logout, Avatarka
+from api.views import UserView, RegisterView,LogView,Logout, Avatarka,GetAvatar
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,4 +10,5 @@ urlpatterns = [
     path('logout/', Logout.as_view()),
     path('addavatar/',Avatarka.as_view()),
     path('deleteavatar/',Avatarka.as_view()),
+    path('uploadavatar/<str:id>',GetAvatar.as_view())
 ]
