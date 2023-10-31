7d724a57ceb44dc68e6fe813f099c85d9f57d4b8
muhammadkhanusmanov <uqaxromon@gmail.com>
2023-10-31 14:27:31 +0500

    enpoint for delete view

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from api.views import UserView, RegisterView,LogView,Logout, AddImage
+from api.views import UserView, RegisterView,LogView,Logout, Avatarka
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,5 +8,6 @@ urlpatterns = [
     path('signup/', RegisterView.as_view()),
     path('login/', LogView.as_view()),
     path('logout/', Logout.as_view()),
-    path('addavatar/',AddImage.as_view())
+    path('addavatar/',Avatarka.as_view()),
+    path('deleteavatar/',Avatarka.as_view()),
 ]
