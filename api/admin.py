b11f55d7da4908f4c4bd066dee26e3b3c82a139a
muhammadkhanusmanov <uqaxromon@gmail.com>
2023-11-02 18:16:47 +0500

    register

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Staff, Avatar
+from .models import Staff, Avatar, Message
 
 admin.site.register(
-    [Staff, Avatar]
+    [Staff, Avatar, Message]
 )
