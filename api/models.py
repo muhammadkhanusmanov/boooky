17ae69ae139059dfa937e037a8d7b631a6e11daa
muhammadkhanusmanov <uqaxromon@gmail.com>
2023-11-02 20:31:35 +0500

    model

--- api/models.py
+++ api/models.py
@@ -17,3 +17,12 @@ class Message(models.Model):
     from_user = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.TextField()
     dt = models.DateTimeField(auto_created=True, auto_now=True)
+
+class TestModel(models.Model):
+    test_id = models.CharField(max_length=6)
+    test_name = models.CharField(max_length=60)
+    tests = models.TextField()
+    answers = models.TextField()
+
+    def __str__(self) -> str:
+        return self.test_name
