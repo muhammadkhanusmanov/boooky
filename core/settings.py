43bd8929a640c2e9b2003429c052f1f14866e8ee
muhammadkhanusmanov <uqaxromon@gmail.com>
2023-11-14 15:50:13 +0500

    setting

--- core/settings.py
+++ core/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = ['boookyuz.pythonanywhere.com']
 # Application definition
 
 INSTALLED_APPS = [
+    'channels',
+    'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -72,7 +74,8 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'core.wsgi.application'
+# WSGI_APPLICATION = 'core.wsgi.application'
+ASGI_APPLICATION = 'core.asgi.application'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
@@ -81,10 +84,16 @@ REST_FRAMEWORK = {
     ],
 }
 
+CORS_ORIGIN_ALLOW_ALL = True
+
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels.layers.InMemoryChannelLayer"
+    }
+}
+
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-CORS_ORIGIN_ALLOW_ALL = True
 
 DATABASES = {
     'default': {
